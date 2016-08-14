d5a063c5c523fb3a933e1b8490a21bab269b4b02
KPicart <kpicart@stepwisedesigns.com>
2016-08-13 23:13:12 -0400

    NSNUmber Class

---
+++ objC/kpWorkingWithNumbers/kpWorkingWithNumbers/main.m
@@ -0,0 +1,75 @@
+//
+//  main.m
+//  kpWorkingWithNumbers
+//
+//  Created by MacMan on 8/13/16.
+//  Copyright © 2016 StepWiseDesigns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        //integer value
+        
+        NSNumber *myNumber, *floatNumber, *intNumber;
+        NSInteger myInt;
+        
+        //intNumber = [NSNumber numberWithInteger:100];
+        //see short hand using '@' to create NSObjects.
+        intNumber =@100;
+        
+        myInt = [intNumber integerValue];
+        NSLog(@"%li",(long)myInt);
+        
+        //long value
+        
+        //myNumber = [NSNumber numberWithLong:0xabcdef];
+        myNumber = @0xabcdef;
+        
+        NSLog(@"%lx",[myNumber longValue]);
+        
+        //char value
+        
+        //myNumber = [NSNumber numberWithChar:88];
+        myNumber = @88;
+        
+        NSLog(@"%c",[myNumber charValue]);
+        
+        
+        //float value
+    
+        //floatNumber = [NSNumber numberWithFloat:100.00];
+        floatNumber = @100.00;
+        
+        NSLog (@"%g",[floatNumber floatValue]);
+        
+        
+        //double
+        
+        myNumber = [NSNumber numberWithDouble:12345e+15];
+        NSLog(@"%lg",[myNumber doubleValue]);
+         
+        //wrong access here
+        
+        NSLog(@"%li",(long)[myNumber integerValue]);
+        
+        //test two numbers for equality
+        
+        if([intNumber isEqualToNumber:floatNumber] == YES)
+            NSLog(@"Numbers are equal");
+        else
+            NSLog(@"Numbers are not equal");
+        
+        //Test of one Number is <,== or > second number
+        
+        if([intNumber compare:myNumber] == NSOrderedAscending)
+            NSLog(@"first number is less than second");
+        
+        
+        
+        
+    }
+    return 0;
+}
